aeed4a6bdf13e39f4415d500c4930e5731138f6d
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-08-12 16:07:25 +0100

    Initial Commit

---
+++ AIPaddyTests/AIPaddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIPaddyTests.swift
+//  AIPaddyTests
+//
+//  Created by Chidume Nnamdi on 12/08/2025.
+//
+
+import Testing
+@testable import AIPaddy
+
+struct AIPaddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
